3c94896b29a93195700cb05b223022a21a40ff62
Vishal Reniguntala <vishalhxc@gmail.com>
2018-11-29 23:45:15 -0600

    Added additional images and updated logos. Right aligned links. Javascript transition background on home page.

---
+++ Bearstrength/Bearstrength/wwwroot/js/Site.js
@@ -0,0 +1,11 @@
+﻿var imageIndex = 1;
+var imageClassCount = 8;
+window.onload = setInterval(rotateHomeBackground, 7000);
+
+function rotateHomeBackground()
+{
+    var homeBackground = document.getElementById("home-background");
+    homeBackground.classList.remove('home-background-image-' + imageIndex);
+    imageIndex = (imageIndex + 1) % imageClassCount;
+    homeBackground.classList.add('home-background-image-' + imageIndex);
+}
